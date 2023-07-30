9cf6f73bf35f34065b9f81ac3b9f58a4d668b566
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-07-30 13:45:07 -0700

    add comments to Screenshot model

--- main_app/models/Screenshot.py
+++ main_app/models/Screenshot.py
@@ -3,16 +3,30 @@ from . import File, Game
 
 class Screenshot(models.Model):
     """
-        Screenshot for a game
+        Model representing a screenshot image file for a game
     """
 
     file = models.OneToOneField(File, on_delete=models.DO_NOTHING)
-    """Associated image file"""
+    """
+        The associated image File
+        Relationship: Screenshot ---- File
+
+        on_delete:
+        When the Screenshot is deleted, we also want to delete its corresponding
+        File. `on_delete` is set to `DO_NOTHING`, because there is no built-in method where 
+        the child deletes the parent when deleted.
+        We manually set the desired behavior in the overridden `Screenshot.delete` def.
+    """
 
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
-    """Associated game"""
+    """
+        The associated Game that owns this Screenshot
+        Relationship: Screenshot >--- Game
+    """
 
     def delete(self, *args, **kwargs):
-        """Clean up file when screenshot is deleted"""
+        """Clean up File when screenshot is deleted"""
         self.file.delete()
+
+        # perform inherited delete
         return super(self.__class__, self).delete(*args, **kwargs)
