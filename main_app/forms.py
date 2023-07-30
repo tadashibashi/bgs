6a491a3ce1467475dd2fec44b7c5a1958ad7a3cc
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-07-30 02:58:14 -0700

    add GameForm

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,3 +1,12 @@
 """
     Custom forms for main_app
 """
+from django.forms import ModelForm
+
+from .models import Game
+
+
+class GameForm(ModelForm):
+    class Meta:
+        model = Game
+        fields = ["title", "description", "tags", "frame_width", "frame_height"]
