404f26ed36dbed8403111261f5690854d05b1a07
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-07-30 03:27:24 -0700

    fix models __init__.py imports

--- main_app/models/__init__.py
+++ main_app/models/__init__.py
@@ -1,2 +1,5 @@
-from .models import Review, Profile, Game, GameScreenshot
 from .File import File
+from .Game import Game
+from .Profile import Profile
+from .Review import Review
+from .Screenshot import Screenshot
