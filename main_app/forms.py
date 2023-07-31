046d9ef827829eb9870a3d9b9bf721d3bad681a7
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-07-30 20:27:19 -0700

    add game file field to GameForm

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,10 +1,11 @@
 """
     Custom forms for main_app
 """
-from django.forms import ModelForm
+from django.forms import ModelForm, FileField
 from .models import Game
 
 class GameForm(ModelForm):
+    game = FileField()
     class Meta:
         model = Game
         fields = ["title", "description", "tags", "frame_width", "frame_height"]
