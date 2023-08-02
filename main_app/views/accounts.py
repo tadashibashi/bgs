be4d2eafa54863598f7e0639854c5a670dbcf47b
cesarjg28 <cesarjg12@yahoo.com>
2023-08-02 14:25:06 -0700

    Added Red Error text for individal sign up+email

--- main_app/views/accounts.py
+++ main_app/views/accounts.py
@@ -1,20 +1,27 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 
+class SignupForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
 def signup(request):
-  error_message = ''
-  if request.method == 'POST':
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-      # Save the user to the db
-      user = form.save()
-      # Automatically log in the new user
-      login(request, user)
-      return redirect('profile_index')
+    error_message = ""
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("profile_index")
+        else: 
+            error_message = "Invalid sign up - try again"
     else:
-      error_message = 'Invalid sign up - try again'
-  # A bad POST or a GET request, so render signup template
-  form = UserCreationForm()
-  context = {'form': form, 'error_message': error_message}
-  return render(request, 'registration/signup.html', context)
+        form = SignupForm()
+    context = {"form": form, "error_message": error_message}
+    return render(request, "registration/signup.html", context)
