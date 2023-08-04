fb100c7034c953f4ae6d8ffce812ed3e6c577386
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-08-04 08:52:18 -0700

    add is_published field to game create form, refactor checkbox field

--- main_app/forms/GameCreateForm.py
+++ main_app/forms/GameCreateForm.py
@@ -3,6 +3,9 @@ from django import forms
 from ..models import Game
 from .TagsField import TagsField
 
+class CheckboxField(forms.BooleanField):
+    widget=forms.CheckboxInput(attrs={"class": "form-check-input ms-2"})
+    required=False
 
 class GameCreateForm(forms.ModelForm):
     """
@@ -12,9 +15,11 @@ class GameCreateForm(forms.ModelForm):
     tags = TagsField(required=False) #ArrayField(CharField(), size=4)
     tags.widget.attrs.update({"placholder": "no tags"})
 
-    add_fullscreen_btn = forms.BooleanField(label="Add fullscreen button?", required=False,
-                                            widget=forms.CheckboxInput(attrs={"class": "form-check-input ms-2"}))
+    add_fullscreen_btn = CheckboxField(label="Add fullscreen button?")
+
+    is_published = CheckboxField(label="Publish")
 
     class Meta:
         model = Game
-        fields = ["title", "description", "tags", "frame_width", "frame_height", "add_fullscreen_btn", "url"]
+        fields = ["title", "description", "tags", "frame_width", "frame_height",
+                  "add_fullscreen_btn", "url", "is_published"]
