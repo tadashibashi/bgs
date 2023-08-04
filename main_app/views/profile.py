7daaf3858f108cfdbb3b1b8391d8eab744e5bda5
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-08-03 23:16:57 -0700

    remove unnecessary json profile in views/profile.py

--- main_app/views/profile.py
+++ main_app/views/profile.py
@@ -6,7 +6,6 @@ from django.shortcuts import render, redirect, get_object_or_404
 from ..forms import ProfileForm
 from ..models import Profile
 
-import json
 
 @login_required
 def index(request: HttpRequest) -> HttpResponse:
