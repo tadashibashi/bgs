dc2f78d3af24cd34dd0c994196741d48459d0ed2
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-08-03 22:53:43 -0700

    add Screenshot to admin portal

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Game, Profile, File, Review, Tag
+from .models import Game, Profile, File, Review, Tag, Screenshot
 
 # add database models to admin portal
 admin.site.register(Game)
 admin.site.register(Profile)
 admin.site.register(Review)
 admin.site.register(File)
+admin.site.register(Screenshot)
 admin.site.register(Tag)
