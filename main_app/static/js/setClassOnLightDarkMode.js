406be561c6af8f646768b8ed544ee61e200093d0
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-08-04 19:32:36 -0700

    refactor light-dark mode button script into utility function

---
+++ main_app/static/js/setClassOnLightDarkMode.js
@@ -0,0 +1,49 @@
+/**
+ * Based on color-mode, set a list of HTMLElements with `ids` to
+ * a class in light mode or dark mode.
+ *
+ * @param elementIds {string[]} list of target element id's
+ * @param darkModeClass {string} class name to set when dark mode
+ * @param lightModeClass {string} class name to set when light mode
+ */
+function setClassOnLightDarkMode(elementIds, darkModeClass, lightModeClass) {
+    const modeBtnEl = document.getElementById('color-mode-btn');
+    if (!modeBtnEl) return; // no mode button available
+
+
+    // get elements from ids
+    const els = [];
+    elementIds.forEach(id => {
+        const btn = document.getElementById(id);
+        if (btn)
+            els.push(btn);
+    });
+
+
+    // theme change callback
+    const handleThemeChange = () => {
+        if (!els.length) return;
+
+        const theme = document.documentElement.getAttribute('data-bs-theme');
+        if (!theme) return; // in case the theme was not set, do not make change
+
+        // set the class based on the current theme
+        if (theme === "dark") {    // on dark mode
+            els.forEach(el => {
+                el.classList.add(darkModeClass);
+                el.classList.remove(lightModeClass);
+            });
+        } else {                   // on light mode
+            els.forEach(el => {
+                el.classList.add(lightModeClass);
+                el.classList.remove(darkModeClass);
+            });
+        }
+    };
+
+    // set the button theme once onload
+    handleThemeChange();
+
+    // and every time the light/dark mode button is clicked
+    modeBtnEl.addEventListener('click', handleThemeChange);
+}
