8f74d67dc439d4f68b1b404387766f4f8043cd98
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-08-06 19:42:00 -0700

    implement search bar, connect to backend api

--- main_app/views/api/search.py
+++ main_app/views/api/search.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.contrib.postgres.search import SearchVector
 from django.http import HttpRequest, HttpResponse, JsonResponse
 
@@ -8,13 +9,16 @@ def search_games(request: HttpRequest) -> HttpResponse:
     params = request.GET
     q = params.get("q", "")
 
-    tokens: list[str] = q.split(" ")
+    tokens: list[str] = [token.lower() for token in q.split(" ")]
     tags = Tag.objects.all()
     filtered_tags = Tag.objects.none()
     filtered_games = Game.objects.none()
+    filtered_users = User.objects.none()
+
     for token in tokens:
         filtered_tags |= tags.filter(text__icontains=token)
         filtered_games |= Game.objects.filter(title__icontains=token)
+        filtered_users |= User.objects.filter(username__icontains=token)
 
     game_ids = {}
     for tag in filtered_tags:
@@ -29,6 +33,13 @@ def search_games(request: HttpRequest) -> HttpResponse:
             game_ids[game.id] = .5
         else:
             game_ids[game.id] += .5
+
+    for user in filtered_users:
+        for game in user.game_set.all():
+            if not game.id in game_ids:
+                game_ids[game.id] = .25
+            else:
+                game_ids[game.id] += .25
 
     game_ids = [item[0] for item in sorted(game_ids.items(), key=lambda k: k[1], reverse=True)]
 
--- main_app/views/games.py
+++ main_app/views/games.py
@@ -2,11 +2,12 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import F
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
 
 from main_app.forms import GameCreateForm
 from main_app.models import Game
 from main_app.models.helpers import create_screenshot, create_or_update_single_screenshot
-
+import requests
 
 def index(request: HttpRequest):
     """
@@ -14,9 +15,27 @@ def index(request: HttpRequest):
         Routing: "games/"
         Name: "games_index"
     """
-    games = Game.objects.all().order_by("-created_at")
+    q = request.GET.get("q", None)
+    games = None
+    if q:
+        try:
+            print(q)
+            game_ids = requests.get(request.scheme + "://" + request.get_host() + reverse("search_games") + "?q=" + q)
+            game_ids = game_ids.json()
+            game_ids = game_ids["games"]
+            print(game_ids)
+            games = Game.objects.filter(id__in=game_ids)
+        except Exception as e:
+            print("Error while searching for games in games index", e)
+            redirect("games_index")
+
+        query_title = "Results for: " + q
+    else:
+        games = Game.objects.all().order_by("-created_at")
+        query_title = "Latest Games"
     return render(request, "games/index.html", {
         "games": games,
+        "query_title": query_title
     })
 
 
