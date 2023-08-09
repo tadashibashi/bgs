229e8b98e55e4be48f08a3354416bd97f30204ed
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-08-08 23:10:48 -0700

    mark screenshots.create as deprecated

--- main_app/views/screenshots.py
+++ main_app/views/screenshots.py
@@ -4,15 +4,20 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect
 from ..models.helpers import create_or_update_single_screenshot
 
-
+# deprecated
 @login_required
 def create(request: HttpRequest, game_id: int) -> HttpResponse:
     """
         Receives a file with input name: "screenshot"
         Replaces or creates a single screenshot for a Game
+
+        Deprecated:
+            No longer using this view. We're using helper functions inside
+            the game create and update views.
     """
     screenshot_file: UploadedFile = request.FILES.get("screenshot", None)
 
-    create_or_update_single_screenshot(screenshot_file, game_id)
+    if screenshot_file:
+        create_or_update_single_screenshot(screenshot_file, game_id)
 
     return redirect("games_detail", pk=game_id)
