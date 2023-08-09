3652025a18d590fe40ca9c4bc38379554e2543ce
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-08-08 23:34:19 -0700

    add docs to clamp function

--- main_app/math.py
+++ main_app/math.py
@@ -1,3 +1,11 @@
 
 def clamp(n: float | int, low: float | int, high: float | int) -> float | int:
+    """
+        Clamps a number between two values inclusively.
+
+        Args:
+            n: number to clamp
+            low: lower limit
+            high: upper limit
+    """
     return max(min(n, high), low)
