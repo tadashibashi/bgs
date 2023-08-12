72ae58f3c177959c33b98f4e57c2b558ae783a4b
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-08-12 15:42:32 -0700

    zip.py: add safe error handling, implement __get_files

--- main_app/util/zip.py
+++ main_app/util/zip.py
@@ -3,6 +3,13 @@
 import typing
 from zipfile import ZipFile
 from pathlib import PurePath
+import re
+
+_IGNORE_LIST = [
+    ".*\.DS_Store",
+    "Thumbs.db",
+    "ehthumbs.*\.db",
+]
 
 class BgsZipfile:
     """
@@ -21,44 +28,92 @@ class BgsZipfile:
 
 
     def open(self, bytes_or_path):
+        """
+            Open a zipfile
+
+            Args:
+                bytes_or_path: a buffer or path to a file
+
+            Returns:
+                True on successful open, and False on error.
+                If an error occurs, internals will not be mutated.
+        """
+
+        # open the file
         try:
             file = ZipFile(bytes_or_path, mode="r")
+        except Exception as e:
+            print("BgsZipfile: failed to open file:", e)
+            return False
+
+
+        try:
+            # test for bad files
             result = file.testzip()
             if result is not None:
-                print(f"Bad file in zip: {result}")
+                print(f"BgsZipFile: bad file in zip: {result}")
                 file.close()
-                return
+                return False
 
+            # clean up old file, if any
             self.close()
+
+            # get files inside zip
+            self.files = self.__get_files(file)
             self.file = file
-            self.__populate_files()
 
         except Exception as e:
-            print("Failed to open zipfile: ", e)
+            print("BgsZipFile: failed during open: ", e)
+            file.close()
+            return False
+
+        return True
 
 
     def is_open(self):
-        return self.file is None
+        """
+            Checks if zip file is open
+        """
+        return bool(self.file and self.file.fp)
 
     def close(self):
-        if self.file and self.file.fp:
-            for file in self.files:
-                file.close()
-            self.files.clear()
+        """
+            Clean up the file. Automatically called during the destructor,
+            but best if called when done with the zip file.
+        """
 
-            self.file.close()
-            self.file = None
+        # handle & report all exceptions, should not throw
+        try:
+            if self.is_open():
+                for file in self.files:
+                    file.close()
+                self.files.clear()
 
-    def __populate_files(self):
-        if not self.is_open():
-            return
+                self.file.close()
+                self.file = None
 
-        path = PurePath(self.file.filename)
+        except Exception as e:
+            print("BgsZipfile: exception occured during close(): ", e)
+
+    @staticmethod
+    def __get_files(zip_file: ZipFile):
+        """
+            Get the individual files from a zip_file
+
+            Returns: array of file objects
+        """
+        path = PurePath(zip_file.filename)
         files = []
 
-        for info in self.file.filelist:
+        for info in zip_file.filelist:
+            filepath = PurePath(info.filename)
+
             # get correct files: no folders or system files
-            if not info.is_dir() and info.filename.startswith(path.stem):
-                files.append(self.file.open(info.filename, "r"))
+            if (not info.is_dir() and filepath.parts[0] == path.stem and
+            not [
+                match for match in _IGNORE_LIST
+                if re.search(match, info.filename)
+            ]):
+                files.append(zip_file.open(info.filename, "r"))
 
-
+        return files
