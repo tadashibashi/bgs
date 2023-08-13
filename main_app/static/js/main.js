31d37762de98472c01b8fe1f7c959f3b3457d0fc
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-08-12 20:59:10 -0700

    bugfix: csrf token error on multiple submits

--- main_app/static/js/main.js
+++ main_app/static/js/main.js
@@ -1,1 +1,15 @@
 // Will be loaded globally in all template files
+
+window.addEventListener("load", evt => {
+    // Prevent forms from being submitted multiple times
+    const forms = document.querySelectorAll("form");
+    for (let i = 0; i < forms.length; ++i) {
+        const form = forms[i];
+        form.addEventListener("submit", evt => {
+            if (form.dataset["attempted"])
+                evt.preventDefault();
+            else
+                form.dataset["attempted"] = "true";
+        });
+    }
+});
