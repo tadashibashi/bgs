b3465eeb7db01949e4471b29053194649c26723c
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-08-13 20:37:20 -0700

    set Favorite fields to foreign keys, actually one-to-many on both ends

--- main_app/models/Favorite.py
+++ main_app/models/Favorite.py
@@ -2,5 +2,5 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Favorite(models.Model):
-    game = models.OneToOneField("Game", on_delete=models.CASCADE)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    game = models.ForeignKey("Game", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
